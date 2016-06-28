87ecc745878eddaa699173cec056e0260f5338f3
Iris Schaffer <irisSchaffer@gmx.net>
2016-06-29 00:19:43 +0200

    Fixes id problem with subslides and fixes emotions on presenter side

--- src/components/controls/reactions/ReactionReceiver.js
+++ src/components/controls/reactions/ReactionReceiver.js
@@ -13,15 +13,28 @@ export default class ReactionReceiver extends React.Component {
   constructor (props) {
     super(props)
 
+    this.getReactions = this.getReactions.bind(this)
+
     this.state = {
-      reaction: null
+      reactions: {}
     }
   }
 
   setup () {
+    const reaction = (code, indices) => {
+      const reactions = this.getReactions(indices)
+
+      return {
+        ...reactions,
+        [code] : reactions[code] + 1 || 1
+      }
+    }
+
     this.observable = Observable.fromEvent(socket, 'state/slide:reaction')
-      .pluck('content')
-      .subscribe((reaction) => this.setState({ reaction }))
+      .subscribe((data) => this.setState({ reactions : {
+        ...this.state.reactions,
+        [data.location.indices[0]]: reaction(data.content, data.location.indices)
+      }}))
   }
 
   tearDown () {
@@ -39,12 +52,22 @@ export default class ReactionReceiver extends React.Component {
     this.setup()
   }
 
+  getReactions (indices = [0]) {
+    let reactions = this.state.reactions[indices[0]] || {}
+
+    return reactions || {}
+  }
+
   render () {
+    const reactions = this.getReactions(this.context.routerState.indices)
     return (
       <div className="reaction-receiver">
-        {this.state.reaction && (
-          <span className="reaction" dangerouslySetInnerHTML={{__html : emotions[this.state.reaction].code}} />
-        )}
+        {Object.keys(reactions).reverse().map((key) => (
+            <span className="reaction" key={key}>
+              <span className="reaction-emotion" dangerouslySetInnerHTML={{__html : emotions[key].code}} />
+              <span className="reaction-count">{reactions[key]}</span>
+            </span>
+        ))}
       </div>
     )
   }
